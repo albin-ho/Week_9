0bc9b79dd521249ff7c900e7a02b3cfdc5e71dae
Albin Ho <albin_ho@yahoo.co.uk>
2025-04-14 14:08:24 +0100

    Uncompleted Task

---
+++ ex3.cpp
@@ -0,0 +1,5 @@
+// Program Purpose:Ex3, Rational Numbers, Too complicated didnt understand the
+// math Author: Last Modified:13/04/2024
+//
+//**********************************************************************************
+#include <iostream>
